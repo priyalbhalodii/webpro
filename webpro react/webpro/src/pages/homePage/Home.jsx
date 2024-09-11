c1f2db7efa81646d7b83f9b7776d0324630cc498
Darshan11777 <dkanani405405@gmail.com>
2024-09-11 15:44:09 +0530

    add our section

---
+++ webpro react/webpro/src/pages/homePage/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import OurSection from '../../component/OurSection'
+import AboutUs from '../../component/AboutUs'
+
+
+export default function Home() {
+  return (
+    <div>
+
+    <OurSection/>
+    <AboutUs/>
+    </div>
+  )
+}
