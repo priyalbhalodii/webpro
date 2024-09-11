b081009f2cfcf399996e197d47b1d8faf2216dc4
priyal28511 <158033740+priyal28511@users.noreply.github.com>
2024-09-11 12:58:25 +0530

    silder update

---
+++ webpro react/webpro/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from './pages/homePage/HeroSection'
+
+export default function App() {
+
+  return (
+    <div>
+      <div>helloo</div>
+      <HeroSection/>
+    </div>
+  )
+}
