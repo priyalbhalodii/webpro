d69a6e3251d0ff960ebc4ac2c618541dc0c8dec7
Darshan11777 <dkanani405405@gmail.com>
2024-09-16 11:43:46 +0530

    change review slider animtion

--- webpro react/webpro/src/component/review/ReviewSlider.jsx
+++ webpro react/webpro/src/component/review/ReviewSlider.jsx
@@ -43,10 +43,10 @@ export default function ReviewSlider() {
               <motion.div
                 key={index}
                 className="absolute w-full h-full flex items-center justify-center"
-                initial={{ opacity: 0.5, scale: 0.8 }}
-                animate={{ opacity: 1, scale: 1 ,z:1}}
-                exit={{ opacity: 0, scale: 0.2,z:-2 }}
-                transition={{ duration: 1 }} // Adjusted duration for smoother transitions
+                initial={{ opacity: 0.5, scale: 0.8, x: 100 }}  // Start off with x translation
+                animate={{ opacity: 1, scale: 1, x: 0, z: 1 }}  // Move back to original position with x: 0
+                exit={{ opacity: 0, scale: 0.2, x: -100, z: -2 }}  // Move out with negative x translation
+                transition={{ duration: 1, delayChildren: 0.2, when: "beforeChildren" }}  // Add delay of initial
               >
                 <ReviewCard />
               </motion.div>
